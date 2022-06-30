f07e9d8eb1de7594c196e60fa9bf837ed4d72160
Biswajit Sahoo <biswa4u85@gmail.com>
2022-06-30 10:30:46 +0530

    upd files

---
+++ src/pages/Layouts/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Outlet, NavLink, useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { Button, DatePicker, Switch, Layout } from 'antd';
+import { useTheme } from "../../theme/use-theme";
+import Footers from "./Footers";
+const { Header, Sider, Content } = Layout;
+
+function Layouts() {
+    const { t, i18n } = useTranslation();
+    let navigate = useNavigate();
+    const [darkMode, setDarkMode] = useTheme();
+    const langChange = () => {
+        let lang = (i18n.language === 'de') ? 'en' : 'de'
+        i18n.changeLanguage(lang)
+    }
+    return (
+        <Layout>
+            <Header>
+                <Button type="primary" onClick={() => langChange()}>LANG {i18n.language}</Button>
+                <Switch checked={darkMode} onChange={setDarkMode} />
+                <Button onClick={() => { navigate(`'/'`) }}>Login Page</Button>
+            </Header>
+            <Content>
+                <Outlet />
+            </Content>
+            <Footers />
+        </Layout>
+    );
+}
+
+export default Layouts;

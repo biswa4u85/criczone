3912e218354efea8f1bfeed0f7dbb8af34747046
Biswajit Sahoo <biswa4u85@gmail.com>
2022-08-12 20:39:43 +0530

    add css

--- src/pages/Scores/HomeSlider.js
+++ src/pages/Scores/HomeSlider.js
@@ -30,10 +30,10 @@ function HomeSlider() {
                 <div className="lanka">
                     <h6>Today At {moment.utc(item.date).format('hh:mm A')} . <span> {item.match_subtitle} .</span> {item.status}</h6>
                     <div className='srilanka'>
-                        <img src={require(`../../assets/flags/Flag of Afghanistan.gif`)} className="flagimg" /> <span> {item?.home?.name}</span>
+                        <img src={require(`../../assets/flags/AFGHANISTAN.gif`)} className="flagimg" /> <span> {item?.home?.name}</span>
                     </div>
                     <div className='srilanka'>
-                        <img src={require(`../../assets/flags/Flag of Afghanistan.gif`)} className="flagimg" /> <span>{item?.away?.name}</span>
+                        <img src={require(`../../assets/flags/AFGHANISTAN.gif`)} className="flagimg" /> <span>{item?.away?.name}</span>
                     </div>
                     <p>Match starts in <span>{moment.utc(item.date).format('Do MMM YYYY hh:mm A')}</span></p>
                     <div className="lanka-border"></div>

0a6a34e6c7bba70e3c2ce9285aa0994f7faa4e04
Biswajit Sahoo <biswa4u85@gmail.com>
2022-08-16 01:18:48 +0530

    add token

--- src/common/Config.js
+++ src/common/Config.js
@@ -5,7 +5,7 @@ export default {
     apiKey: 'RS5:3b28e1af862fe552f9cade04db1a8705',
     frappe_custom_app: 'erp_custom_auth',
     frappe_url: 'https://news.techwizards.io',
-    token: 'token 8013775618bd3a7:8fa905e93b04d2b',
+    token: 'token 8013775618bd3a7:99fa9ff03295aa8',
     rapidAPIHost: 'cricket-live-data.p.rapidapi.com',
     rapidAPIKey: 'ceff4f9011mshb4a79448651b42dp119c71jsn663c2e90ee8a',
     rapidAPIUrl: 'https://cricket-live-data.p.rapidapi.com',

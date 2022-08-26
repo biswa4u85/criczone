3437b55dc2710f7c3d61492324934bc2f0715f8c
Biswajit Sahoo <biswa4u85@gmail.com>
2022-08-26 10:54:47 +0530

    fixed UI

--- src/common/Flags.js
+++ src/common/Flags.js
@@ -1,5 +1,5 @@
 export default {
     NoImg: require("../assets/flags/NOIMG.png"),
-    Italy: require("../assets/flags/ITALY.gif"),
-    Italy: require("../assets/flags/ITALY.gif"),
+    Italy: require("../assets/flags/Italy.gif"),
+    Italy: require("../assets/flags/Italy.gif"),
 }

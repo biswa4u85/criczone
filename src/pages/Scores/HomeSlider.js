c1e945c1ea426bbff69552309f683ef9808e585d
avijit <avijit.erptech@gmail.com>
2022-10-19 17:33:30 +0530

    home slider change digain

--- src/pages/Scores/HomeSlider.js
+++ src/pages/Scores/HomeSlider.js
@@ -3,9 +3,11 @@ import OwlCarousel from "react-owl-carousel";
 import moment from "moment";
 import Config from '../../common/Config'
 import Flags from "../../common/Flags";
+import { Col, Row, Image } from 'antd';
 import { useSelector, useDispatch } from 'react-redux'
 import SocketApis from '../../utility/socket-apis'
 import { getHomeFixtures } from "../../store/ScoreRedux";
+import Flag from '../../assets/image/flag.png';
 
 const responsive = {
     0: {
@@ -55,13 +57,13 @@ function HomeSlider(props) {
     if (fixtures.length === 0) {
         return null
     }
-   
+
 
     return (<OwlCarousel className='owl-theme' responsive={responsive} margin={9} autoplay={true} nav={false}>
         {fixtures.map((item, key) => {
             let score = null
             return <div key={key} id={`live_home_${item.name}`} className='item'>
-                <div className="trending_news">
+                {/* <div className="trending_news">
                     <div className="lanka" onClick={() => navigate(`/match-news/${item.name}`)}>
                         <h6>{Config.checkDate(item.date)} At {moment.utc(item.datetime).format('hh:mm A')} . <span> {item.match_subtitle} .</span> {item.status}</h6>
                         <div className='srilanka'>
@@ -76,7 +78,50 @@ function HomeSlider(props) {
                             <li>{item.venue}</li>
                         </ul>
                     </div>
+                </div> */}
+                <div className="finshed">
+                    <div className="lanka">
+                        <Row>
+                            <Col span={9}>
+                                <h5>FINISHED</h5>
+                            </Col>
+                            <Col span={7} offset={8}>
+                                <h6>Today 07:03 am</h6>
+                            </Col>
+                        </Row>
+                        <div className="border-box"></div>
+                        <div className="false">
+                            <div className="false">
+                                <Image preview={false} src={Flag} />
+                                <h6>NZ</h6>
+                            </div>
+                            <h6><span>(ov 20/20)</span>280/5</h6>
+                        </div>
+
+                        <div className="false">
+                            <div className="false">
+                                <Image preview={false} src={Flag} />
+                                <h6>BAN</h6>
+                            </div>
+                            <h6><span>(ov 20/20)</span>160/7</h6>
+                        </div>
+                        <h5>New Zealand bate Bangladesh by 48 runs</h5>
+                    </div>
+
+                    <div className="false-zealand">
+                        <Row>
+                            <Col span={12}>
+                                <h5>View Details</h5>
+                            </Col>
+                            <Col span={12} >
+                                <h6>New Zealand T20 Tri-Series</h6>
+                            </Col>
+                        </Row>
+                    </div>
+
                 </div>
+
+
             </div>
         })}
     </OwlCarousel>);
